1a72916c8195aaa428c89fa8df91f9df0a894de4
Yaniv Kessler <yanivk@gmail.com>
2022-05-04 00:07:40 +0300

    chore: correct name

--- catchart.js
+++ catchart.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const chartcat = require('./index')
+const catchart = require('./index')
 const StreamSlicer = require('stream-slicer')
 const { pipeline } = require('stream')
 const program = require('./program')
 
 let slicer = new StreamSlicer({ sliceBy: program.rowSeparator })
-let chartStream = chartcat(program)
+let chartStream = catchart(program)
 
 pipeline(process.stdin, slicer, chartStream, err => {
 	if (err) {
