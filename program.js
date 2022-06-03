7d572afcd660514278cf1621d44ea601dc539825
Yaniv Kessler <yanivk@gmail.com>
2022-06-03 18:27:31 +0300

    bug: fix command line config issues

--- program.js
+++ program.js
@@ -11,20 +11,20 @@ program.version(version)
 	.option('--dataField <dataField>', 
 		`When the input format is json, this option will specify which field in the row is the source of the data.
 This can be a singular value or an array. When "auto" is set, these fields will be "value" or "data". 
-When input format is csv dataField is ignored`, 'auto')
+When input format is csv dataField is ignored`, config.dataField)
 	.option('--labelSource <labelSource>', 
 `When the input format is json, labels are obtained from each row using a specific field, specified through this option.
 While this option is set to "auto" then label will be obtained from "label" or "title" or "key.
 When the input format is csv and labelSource is set to "auto" then the label will be the time difference from the start
 to the data point. Setting labelSource to "row" will case the label to be extracted from the data, specifically the first
-value in each row will be considered the label`, 'auto')
+value in each row will be considered the label`, config.labelSource)
 	.option('--showValueLabels', 'show labels on values in the chart', config.showValueLabels)
 	.option('--noFill', 'Do not fill the area under the chart line with color', config.noFill)
 	.option('--usePatterns', 'Fill the area under the chart line with a pattern, this is meant for individuals who suffer from color blindness', config.usePatterns)
 	.option('--fieldCount <fieldCount>', 'The number of fields that are piped to catchart in each row, "auto" will try to deduce this value from the first row of data', config.fieldCount)
 	.option('--port <port>', 'hcat config: http port to use, defaults to a randomly picked port', 0)
-	.option('--hostname <hostname>', 'hcat config: hostname of the http server', 'localhost')
-	.option('--contentType <contentType>', 'hcat config: content type of the response', 'text/html')
+	.option('--hostname <hostname>', 'hcat config: hostname of the http server', config.hostname)
+	.option('--contentType <contentType>', 'hcat config: content type of the response', config.contentType)
 	.option('--shutdownDelay <millis>', 'delay (in ms) before shutting down server once ths input stream ended', 1000)
 	.parse(process.argv)
 
